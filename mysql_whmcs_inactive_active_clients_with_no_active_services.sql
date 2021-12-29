1979b964d915fb9f6bd9b272b477e79a1c9f68ea
SBZ systems <support@sbzsystems.com>
2021-12-29 19:25:14 +0200

    Update mysql_whmcs_inactive_active_clients_with_no_active_services.sql

--- mysql_whmcs_inactive_active_clients_with_no_active_services.sql
+++ mysql_whmcs_inactive_active_clients_with_no_active_services.sql
@@ -1,7 +1,9 @@
+
 update tblclients set tblclients.status='Inactive'
 WHERE
-    
-    (select count( tblhosting.id) from tblhosting where tblclients.id=tblhosting.userid)=0
-     and
-    (select count( tbldomains.id) from tbldomains where tblclients.id=tbldomains.userid)=0
-     and tblclients.status='Active'
+        
+(select count( tblhosting.id) from tblhosting where tblclients.id=tblhosting.userid and tblhosting.domainstatus='Active')=0
+and  
+(select count( tbldomains.id) from tbldomains where tblclients.id=tbldomains.userid and tbldomains.status='Active')=0  
+
+and tblclients.status='Active'
