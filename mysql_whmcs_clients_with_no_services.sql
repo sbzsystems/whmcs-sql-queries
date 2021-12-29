7bb3f30e4309980b91f311ff11434be5dff78ddd
SBZ systems <support@sbzsystems.com>
2021-12-29 19:14:59 +0200

    Update mysql_whmcs_clients_with_no_services.sql

--- mysql_whmcs_clients_with_no_services.sql
+++ mysql_whmcs_clients_with_no_services.sql
@@ -3,6 +3,6 @@
 SELECT * FROM tblclients
 WHERE
     
-    (select count( tblhosting.id) from tblhosting where tblclients.id=tblhosting.userid limit 1)=0
+    (select count( tblhosting.id) from tblhosting where tblclients.id=tblhosting.userid)=0
      and
-    (select count( tbldomains.id) from tbldomains where tblclients.id=tbldomains.userid limit 1)=0
+    (select count( tbldomains.id) from tbldomains where tblclients.id=tbldomains.userid)=0
