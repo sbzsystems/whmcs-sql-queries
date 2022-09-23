5d80f8bfd95b022fe493ee1273dc4fc888f00490
SBZ systems <support@sbzsystems.com>
2022-09-23 15:01:13 +0300

    Update firebird_emdi_doc_abbreviation.sql

--- firebird_emdi_doc_abbreviation.sql
+++ firebird_emdi_doc_abbreviation.sql
@@ -1,21 +1,30 @@
 create or alter function DOC_ABBREVIATION (
-DOCTITLE varchar(1000),
-DOCNUMBER varchar(1000))
+    DOCTITLE varchar(1000),
+    DOCNUMBER varchar(1000))
 returns varchar(1000)
 as
+declare q varchar(100);
+declare i int;
+declare countc int;
 begin
-return
-replace(
-replace(
-replace(
-left(split_string(DOCTITLE,' ',1),1)||
-left(split_string(DOCTITLE,' ',2),1)||
-left(split_string(DOCTITLE,' ',3),1)
-,'(','')
-,')','')
-,' - ','')
-||' '
-||DOCNUMBER;
-end;
 
--- 0 row(s) affected.
+q=DOCTITLE;
+
+i=1;
+countc=0;
+while (i<=char_length(q)) do begin
+    if (substring(q from i for 1) = ' ') then countc=countc+1;
+    i=i+1;
+end
+
+q=replace(q,'(','');
+q=replace(q,')','');
+q=replace(q,' - ','');
+
+if (countc=0) then q=left(split_string(q,' ',1),3)||' '||DOCNUMBER;
+if (countc=1) then q=left(split_string(q,' ',1),1)||left(split_string(q,' ',2),3)||' '||DOCNUMBER;
+if (countc>1) then q=left(split_string(q,' ',1),1)||left(split_string(q,' ',2),1)||left(split_string(q,' ',3),1)||' '||DOCNUMBER;
+
+return q;
+
+end
